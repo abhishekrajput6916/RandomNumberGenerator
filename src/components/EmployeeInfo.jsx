9a2bebbb02c91f67a4b6cd69105c810b79f84504
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2025-01-10 00:29:29 +0530

    minor

--- src/components/EmployeeInfo.jsx
+++ src/components/EmployeeInfo.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function EmployeeInfo({activeEmployee}) {
+function EmployeeInfo({ activeEmployee }) {
   return (
     <div className='employee-container'>
-    <div className="employee-info ">
-   <div>Name : <span>{ activeEmployee.FIRST_NAME}</span></div>
-   <div>Email : <span>{ activeEmployee.EMAIL}</span></div>
-   <div>Salary : <span>{ activeEmployee.SALARY}</span></div>
-   <div>ID : <span>{ activeEmployee.id}</span></div>
-   </div>
-</div>
+      <div className="employee-info ">
+        <div>Name : <span>{activeEmployee.FIRST_NAME}</span></div>
+        <div>Email : <span>{activeEmployee.EMAIL}</span></div>
+        <div>Salary : <span>{activeEmployee.SALARY}</span></div>
+        <div>ID : <span>{activeEmployee.id}</span></div>
+      </div>
+    </div>
   )
 }
 
